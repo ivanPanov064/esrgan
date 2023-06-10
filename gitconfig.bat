631f8b1225c56f202074d1315da1537ed9b904b3
Ivan <ivanpanov064@gmail.com>
2023-06-10 16:54:18 +0200

    Initial commit

---
+++ gitconfig.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set gitName="Ivan Panov"
+git config --global user.name "%gitName%"
+
+set gitEmail="ivanpanov064@gmail.com"
+git config --global user.email "%gitEmail%"
+
+echo Git configuration completed.
